04f973d52c41f15d038fcc428a14424dafefb16b
Igor Zubkov <icesik@altlinux.org>
2012-05-30 21:27:12 +0300

    Add factory for Repository

---
+++ spec/factories/application.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :repository do
+  	factory :sisyphus_repository do
+      name 'Sisyphus'
+      short_url 'sisyphus'
+      vendor 'ALT Linux Team'
+      distribution 'ALT Linux'
+      download_path '/Sisyphus'
+      order_id
+    end
+  end
+
+  sequence :order_id do |n|
+    "#{n}"
+  end
+end

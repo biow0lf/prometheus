31a14132449a465e93e149da3ec1623f58123ff0
Igor Zubkov <icesik@altlinux.org>
2012-05-30 19:32:32 +0300

    Re-init prometheus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prometheus::Application.config.secret_token = '5ab5d47f73d67f2115ff3940abc6005bfb6f56cc4d95b861ba6eed011303b1d6bfe23bce9d6a44e9f6236bae1f246a27646652028a1bcbce054193f6f7990e36'

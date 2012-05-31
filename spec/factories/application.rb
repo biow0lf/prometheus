d874cc7d788495f0512a37096269b8530041d7ed
Igor Zubkov <icesik@altlinux.org>
2012-05-31 22:09:14 +0300

    Replace tabs with spaces

--- spec/factories/application.rb
+++ spec/factories/application.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :repository do
-  	factory :sisyphus_repository do
+    factory :sisyphus_repository do
       name 'Sisyphus'
       short_url 'sisyphus'
       vendor 'ALT Linux Team'
@@ -14,5 +14,18 @@ FactoryGirl.define do
 
   sequence :order_id do |n|
     "#{n}"
+  end
+
+  factory :maintainer do
+    factory :maintainer_icesik do
+      name 'Igor Zubkov'
+      email 'icesik@altlinux.org'
+      login 'icesik'
+      time_zone 'UTC'
+      jabber 'icesik@altlinux.org'
+      info 'Lorem Ipsum'
+      website 'http://packages.altlinux.org/'
+      location 'Donetsk, Ukraine'
+    end
   end
 end

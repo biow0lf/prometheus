b4e6aef979df6abfe5edbd8bdf8668c73a4f5ac6
Igor Zubkov <icesik@altlinux.org>
2012-05-31 21:07:04 +0300

    Add Repocop.update_repocop_cache (disable right now)

--- app/models/repocop.rb
+++ app/models/repocop.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-require 'open-uri'
+# require 'open-uri'
 
 class Repocop < ActiveRecord::Base
   attr_accessible :arch, :message, :name, :release, :repository_id, :srcname, :srcrel, :srcversion, :status, :testname, :version
@@ -20,13 +20,34 @@ class Repocop < ActiveRecord::Base
   # def self.update_repocop
   #   ActiveRecord::Base.transaction do
   #     Repocop.delete_all
-
+  #
   #     url = "http://repocop.altlinux.org/pub/repocop/prometeus2/prometeus2.sql"
   #     file = open(URI.escape(url)).read
-
+  #
   #     file.each_line do |line|
   #       ActiveRecord::Base.connection.execute(line)
   #     end
+  #   end
+  # end
+  #
+  # def self.update_repocop_cache
+  #   branch = Branch.where(vendor: 'ALT Linux', name: 'Sisyphus').first
+  #   srpms = branch.srpms.all
+  #   srpms.each do |srpm|
+  #     repocops = Repocop.where(srcname: srpm.name,
+  #                              srcversion: srpm.version,
+  #                              srcrel: srpm.release).all
+  #
+  #     repocop_status = 'skip'
+  #     repocops.each do |repocop|
+  #       repocop_status = 'ok' if repocop.status == 'ok' and repocop_status != 'info' and repocop_status != 'experimental' and repocop_status != 'warn' and repocop_status != 'fail'
+  #       repocop_status = 'info' if repocop.status == 'info' and repocop_status != 'experimental' and repocop_status != 'warn' and repocop_status != 'fail'
+  #       repocop_status = 'experimental' if repocop.status == 'experimental' and repocop_status != 'warn' and repocop_status != 'fail'
+  #       repocop_status = 'warn' if repocop.status == 'warn' and repocop_status != 'fail'
+  #       repocop_status = 'fail' if repocop.status == 'fail'
+  #     end
+  #     srpm.repocop = repocop_status
+  #     srpm.save!
   #   end
   # end
 end
